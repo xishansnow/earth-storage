34611f662d1fec537505a5b54c5aace61cf70491
Admin <8987719@qq.com>
2020-12-31 22:32:16 +0800

    Add some partitioner type(no implements)

---
+++ README.md
@@ -0,0 +1,24 @@
+
+
+
+#Prerequisite:
+
+  Beast is a package for spatial big data distribution storage & parallel processing.  For its version on maven repository isn't the latest. It may good to compile it yourself.
+  (1) Compile Beast packages        
+        You can find beast on https://bitbucket.org/eldawy/beast/src/master/. Clone it and compile with instruction in README.md.
+  (2) Install Beast jars to local maven repository.
+        Go to the directory which contain beast-*-*.jar, and use 'mvn install' command like below.
+        mvn install:install-file -Dfile=beast-uber-spark-0.9.0-SNAPSHOT.jar -DgroupId=edu.ucr.cs.bdlab -DgroupId=edu.ucr.cs.bdlab -DartifactId=beast -Dversion=0.9.0 -Dpackaging=jar
+    (3) Test Beast 
+        In your project, edit POM.xml, add dependency like that:
+'''
+            <dependencies>
+                <dependency>
+                    <groupId>edu.ucr.cs.bdlab</groupId>
+                    <artifactId>beast</artifactId>
+                    <version>0.9.0</version>
+                </dependency>
+            </dependencies>
+''''
+        In your code file, add codes to test if beast can be used: 
+        import edu.ucr.cs.bdlab.beast.common.BeastOptions;
--- indexing/src/main/java/cn/edu/pku/coe/asic/indexing/DggsPartitioner.java
+++ indexing/src/main/java/cn/edu/pku/coe/asic/indexing/DggsPartitioner.java
@@ -1,46 +1,33 @@
 package cn.edu.pku.coe.asic.indexing;
 
-import edu.ucr.cs.bdlab.indexing.SpatialPartitioner;
-import edu.ucr.cs.bdlab.stsynopses.AbstractHistogram;
-import edu.ucr.cs.bdlab.stsynopses.Summary;
-import org.apache.hadoop.conf.Configuration;
-import edu.ucr.cs.bdlab.geolite.EnvelopeND;
+
+import edu.ucr.cs.bdlab.beast.cg.SpatialPartitioner;
+import edu.ucr.cs.bdlab.beast.common.BeastOptions;
+import edu.ucr.cs.bdlab.beast.geolite.EnvelopeND;
+import edu.ucr.cs.bdlab.beast.geolite.EnvelopeNDLite;
+import edu.ucr.cs.bdlab.beast.synopses.AbstractHistogram;
+import edu.ucr.cs.bdlab.beast.synopses.Summary;
+import edu.ucr.cs.bdlab.beast.util.IntArray;
+
 
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
+import java.io.PrintStream;
+import java.util.Iterator;
 
 
-public class DggsPartitioner implements  SpatialPartitioner {
+public abstract class DggsPartitioner implements  SpatialPartitioner {
 
     @Override
-    public void setup(Configuration conf, boolean disjoint, PartitionCriterion pc, long value) {
+   public void setup(BeastOptions conf, boolean disjoint) {
+
 
     }
 
     @Override
-    public void construct(Summary summary, double[][] doubles, AbstractHistogram abstractHistogram) {
+    public void construct(Summary summary, double[][] doubles, AbstractHistogram abstractHistogram, int i) {
+
 
     }
-
-    @Override
-    public void writeExternal(ObjectOutput out) throws IOException {
-
-    }
-
-    @Override
-    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-
-    }
-
-    @Override
-    public int getPartitionCount() {
-        return 0;
-    }
-
-    @Override
-    public int getCoordinateDimension() {
-        return 0;
-    }
-
 }
